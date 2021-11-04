012f809c27da2430391c2f09b4cc2726c789e7ec
jarnolooij <JarnoLooij@gmail.com>
2021-11-04 12:46:31 +0100

    add main menu

---
+++ All For One/Assets/Scripts/Player1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    private string playerName;
+    private int money;
+
+    List<GameObject> units = new List<GameObject>();
+}
