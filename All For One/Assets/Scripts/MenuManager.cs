84a17decfb81e8d456033786cbea85d38573c9e3
jarnolooij <JarnoLooij@gmail.com>
2021-11-04 14:01:39 +0100

    update

--- All For One/Assets/Scripts/MenuManager.cs
+++ All For One/Assets/Scripts/MenuManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject MainMenu;
+    public GameObject InsertPlayerNames;
+    public GameObject inputField1;
+    public GameObject inputField2;
+    string username1;
+    string username2;
+    public void getValuePlayerName1()
     {
-        
+        username1 = inputField1.GetComponent<TMP_InputField>().text;
+    }
+    public void getValuePlayerName2()
+    {
+        username2 = inputField2.GetComponent<TMP_InputField>().text;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        MainMenu.SetActive(true);
+        InsertPlayerNames.SetActive(false);
     }
+
+    public void StartGame()
+    {
+        MainMenu.SetActive(false);
+        InsertPlayerNames.SetActive(true);
+    }
+
+    public void PlayGame()
+    {
+        Debug.Log(username1);
+        Debug.Log(username2);
+        SceneManager.LoadScene("PlayGround");
+    }
+    public void Back()
+    {
+        MainMenu.SetActive(true);
+        InsertPlayerNames.SetActive(false);
+    }
+
+
 }
